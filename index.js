a810b1dbc3cfaf0ee4fb9cb672f3e4b8ac0d78de
WooRocket <contato@woorocket.com.br>
2022-08-22 08:52:23 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,83 @@
+<!-- View content -->
+<script>
+  fbq('track', 'ViewContent', {
+    value: 0.01,
+    currency: 'BRL',
+  });
+</script>
+ _____________________________________________
+
+**Add to cart**
+<script>
+  fbq('track', 'AddToCart', {
+    value: 0.01,
+    currency: '0.01',
+  });
+</script>
+
+**Iniciar finalização de compra**
+<script>
+fbq('track', 'InitiateCheckout', {
+value: 0.01,
+currency: 'BRL',
+});
+</script>
+_____________________________________________
+
+**Adicionar formas de pagamento**
+<script>
+  fbq('track', 'AddPaymentInfo');
+</script> 
+_____________________________________________
+
+**Compra Concluída**
+<script>
+  fbq('track', 'Purchase', {
+    value: 0.01,
+    currency: 'BRL',
+  });
+</script>
+
+_____________________________________________
+
+**Registro completo**
+<script>
+  fbq('track', 'CompleteRegistration', {
+    value: 0.01,
+    currency: 'BRL',
+  });
+</script>
+
+**Contato**
+<script>
+  fbq('track', 'Contact');
+</script>
+
+Cadastro**
+<script>
+  fbq('track', 'Lead', {
+    value: 0.01,
+    currency: 'BRL',
+  });
+</script>
+
+**Assinar**
+<script>
+  fbq('track', 'Subscribe', {
+    value: 0.01,
+    currency: 'BRL',
+  });
+</script>
+
+**Encontrar Localização**
+<script>
+  fbq('track', 'FindLocation');
+</script>
+
+**Pesquisar**
+<script>
+  fbq('track', 'Search', {
+    search_string: '0.01',
+    content_ids: '0.01',
+  });
+</script>
